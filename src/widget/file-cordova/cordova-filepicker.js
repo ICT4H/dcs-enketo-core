95bd48c55d9efcde358530a5b0c0c7eb975b1d24
Yogesh Sreedharan <yogeshsr@thoughtworks.com>
2014-10-07 16:00:33 +0530

    Yogesh || Custom buttons to add photo, audio & video from device to submission

--- src/widget/file-cordova/cordova-filepicker.js
+++ src/widget/file-cordova/cordova-filepicker.js
@@ -1,4 +1,4 @@
-define( [ 'jquery', 'enketo-js/Widget', 'cordovaMediaManager' ], function( $, Widget, cordovaMediaManager ) {
+define( [ 'jquery', 'enketo-js/Widget', 'fileSystem', 'deviceHandler' ], function( $, Widget, fs, deviceHandler ) {
     "use strict";
 
     var pluginName = 'cordovaFilepicker';
@@ -24,127 +24,75 @@ define( [ 'jquery', 'enketo-js/Widget', 'cordovaMediaManager' ], function( $, Wi
      *
      */
     CordovaFilepicker.prototype._init = function() {
-        var $input = $( this.element ),
-            existingFileName = $input.attr( 'data-loaded-file-name' ),
+        var $fileInput = $( this.element ),
+            existingFileName = $fileInput.attr( 'data-loaded-file-name' ),
             that = this;
-        
-        this.mediaType = $input.attr( 'accept' );
 
-        $input
+        this.mediaType = $fileInput.attr( 'accept' );
+        console.log('mediatype: ' + this.mediaType)
+
+        initViewElements(this);
+
+        console.log('existingFileName : ' + existingFileName);
+        if ( existingFileName ) {
+            this._showFileName(existingFileName);
+            fs.fileNameToURL(existingFileName, function(url) {
+                that._showPreview(url, that.mediaType);
+            });
+        }
+
+        addMediaButtons(this, fs);
+    };
+
+    function initViewElements(that) {
+        var $fileInput = $( that.element );
+        $fileInput
             .attr( 'disabled', 'disabled' )
             .addClass( 'transparent' )
             .parent().addClass( 'with-media clearfix' );
 
-        this.$widget = $(
+        that.$widget = $(
             '<div class="widget file-picker">' +
             '<div class="media-buttons"></div>' +
             '<div class="fake-file-input"></div>' +
             '<div class="file-feedback"></div>' +
             '<div class="file-preview"></div>' +
             '</div>' )
-            .insertAfter( $input );
-        this.$feedback = this.$widget.find( '.file-feedback' );
-        this.$mediaButtons = this.$widget.find( '.media-buttons' );
-        this.$preview = this.$widget.find( '.file-preview' );
-        this.$fakeInput = this.$widget.find( '.fake-file-input' );
+            .insertAfter( $fileInput );
+        that.$feedback = that.$widget.find( '.file-feedback' );
+        that.$buttonContainer = that.$widget.find( '.media-buttons' );
+        that.$preview = that.$widget.find( '.file-preview' );
+        that.$fakeInput = that.$widget.find( '.fake-file-input' );
+    }
 
-        console.log('existingFileName : ' + existingFileName);
-        // show loaded file name regardless of whether widget is supported
-        if ( existingFileName ) {
-            this._showFileName(existingFileName);
-            $input.removeAttr( 'data-loaded-file-name' );
-            cordovaMediaManager.fileNameToURL(existingFileName, function(url) {
-                that._showPreview(url, that.mediaType);    
-            });
+    function addMediaButtons(that, fs) {
+        // TODO based on media type do the navigator check
+        if ( navigator && navigator.device.capture.captureVideo ) {
             
+            var mediaButton = new MediaButton(deviceHandler, fs);
+            
+            mediaButton.addButtons(that.$buttonContainer, that.mediaType, {
+                success: function(fileUrl, fileName) {
+                    updateView(that, fileUrl, fileName);
+                }, 
+                error: function() {
+                    //TODO fix when fail to load media, but previous preview & file_name exists
+                    that._showFeedback( 'Media not selected.', 'warning' );
+                }
+            });
         }
-
-        //TODO enable this message
-        // if ( !fileManager || !fileManager.isSupported() ) {
-        //     this._showFeedback( 'File Uploads not supported in this browser.', 'warning' );
-        //     return;
-        // }
-
-        //TODO move adding listerner after mediaMgr is ready
-        // this._changeListener();
-
-        if ( navigator && navigator.camera ) {
-            //TODO hide/disable the input file. Check model is updated, as it may not pick the disabled inputs.
-            addCustomMediaButtons(that);
-        }
-    };
-
-    function addCustomMediaButtons(that) {
-        addCameraHandlers(that);
-        addGalleryHandlers(that);
     }
 
-    function addCameraHandlers(that) {
-        var callbacks = _getFileSeletedCallbacks(that);
-
-        var captureButton = createCameraButton(callbacks);
-        that.$mediaButtons.append(captureButton);
-    }
-
-    function addGalleryHandlers(that) {
-        var callbacks = _getFileSeletedCallbacks(that);
-
-        var galleryButton = createGalleryButton(callbacks);
-        that.$mediaButtons.append(galleryButton);
-    }
-
-    function _getFileSeletedCallbacks(that) {
-        return {
-            success: function(newFileUrl, newFileName) {
-                updateView(that, newFileUrl, newFileName)
-            }, error: function() {
-                console.log('error in postCapture');
-            }
-        };
-    }
-
-    function createCameraButton(callbacks) {
-        return getButton({
-            label: 'Camera',
-            clickHandler: function() {
-                cordovaMediaManager.captureAndMove(callbacks);
-            }
-        });
-    }
-
-    function createGalleryButton(callbacks) {
-        return getButton({
-            label: 'Gallery',
-            clickHandler: function() {
-                cordovaMediaManager.copyFromGallery(callbacks);
-            }
-        });
-    }
-
-    function getButton(options) {
-        return $('<button />', {
-            type  : 'button',
-            html : options.label,
-            id    : 'btn_capture',
-            on    : {
-                click: options.clickHandler
-            }
-        });
-    }
-
-    function updateView(that, imageUrl, newFileName) {
+    function updateView(that, mediaUrl, fileName) {
         console.log('in updateView');
         var $input = $( that.element );
-        console.log('input: '+that.element);
-        $input.attr('data-previous-file-name', newFileName);
-        console.log('data-previous-file-name: '+newFileName);
-        that._showPreview( imageUrl, that.mediaType );
-        console.log('imageUrl: '+ imageUrl);
+
+        $input.attr('data-previous-file-name', fileName);
+        $input.removeAttr( 'data-loaded-file-name' );
+        that._showPreview( mediaUrl, that.mediaType );
         that._showFeedback( '' );
-        that._showFileName( newFileName );
-        console.log('newFileName: '+newFileName);
+        that._showFileName( fileName );
         $input.trigger( 'change.file' );
-        console.log('triggered change.file');
 
     }
 
@@ -153,20 +101,6 @@ define( [ 'jquery', 'enketo-js/Widget', 'cordovaMediaManager' ], function( $, Wi
         return maxSize || 5 * 1024 * 1024;
     };
 
-    // CordovaFilepicker.prototype._changeListener = function() {
-    //     var $input = $( this.element );
-    //     $input.on( 'change.passthrough.' + this.namespace, function( event ) {
-            
-    //         //console.debug( 'namespace: ' + event.namespace );
-    //         if ( event.namespace === 'passthrough' ) {
-    //             //console.debug('returning true');
-    //             $input.trigger( 'change.file' );
-    //             return false;
-    //         }
-            
-    //         return true;
-    //     } );
-    // };
 
     CordovaFilepicker.prototype._showFileName = function( file ) {
         var fileName = ( typeof file === 'object' && file.name ) ? file.name : file;
